027b71628c14ab73713c94cb954530d95b8aeaec
OlgaB <olga.bereznaja@gmail.com>
2020-06-26 22:17:13 +0100

    Lesson-10 done

--- script.js
+++ script.js
@@ -1,65 +1,56 @@
 'use strict';
 
-/*
 
- */
+//1 Восстановить порядок книг.
 
-const gameBot = function () {
-  //Проверка, являются ли введенные данные- числом.
-  const isNumber = (num) => {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  };
-  //Создание функции случайного числа
-  let createRandomNumber = () => {
-    return Math.ceil(Math.random() * 100);
-  };
-  //Вызов случайного числа и присвоение результата переменной
-  let botNumber = createRandomNumber();
-  console.log('botNumber: ', botNumber);
+const allBooks = document.querySelectorAll('.book');
+
+allBooks[0].before(allBooks[1]);
+allBooks[5].after(allBooks[2]);
+allBooks[0].after(allBooks[4]);
 
 
-  let getUserResponse = () => {
-    let usersResponse = prompt('Угадай число от 1 до 100');
+//2 Заменить картинку заднего фона на другую из папки image
 
-    if (usersResponse === null) {
-      //— если пользователь нажимает "Отмена", то игра заканчивается.
-      alert('Game over');
-      console.log('Game over');
-      return false;
+const bodyContent = document.querySelector('body');
+bodyContent.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
-    } else if (isNumber(usersResponse)) {
-      if (+usersResponse === botNumber) {
-        alert('Успех!');
-        console.log('Успех!');
-        return true;
 
-      } else if (+usersResponse < botNumber) {
-        //— если пользовательское число меньше, 
-        //то бот выводит "Загаданное число больше" и предлагает ввести новый вариант;
-        alert('Загаданное число больше');
-        console.log('Загаданное число больше');
-        return getUserResponse();
+//3 Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
 
-      } else if (+usersResponse > botNumber) {
-        //— если пользовательское число больше, 
-        //то бот выводит "Загаданное число меньше" и предлагает ввести новый вариант;
-        alert('Загаданное число меньше');
-        console.log('Загаданное число меньше');
-        return getUserResponse();
-      }
+const bookThreeHeading = allBooks[4].querySelector('h2');
+const newHeadingbookThree = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a>';
+bookThreeHeading.innerHTML = newHeadingbookThree;
 
-    } else if (!isNumber(usersResponse)) {
-      //— если пользователь ввел не число, 
-      //то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
-      alert('Введи число!');
-      console.log('Введи число!');
-      return getUserResponse();
-    }
-  };
 
-  return getUserResponse;
-};
+//4 Удалить рекламу со страницы
+const advert = document.querySelector('.adv');
+advert.remove();
 
-let initiateGame = gameBot();
-initiateGame();
-// console.dir(initiateGame);
+
+//5 Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools).
+
+const chaptersBookTwo = allBooks[0].querySelectorAll('li');
+
+chaptersBookTwo[7].after(chaptersBookTwo[2]);
+chaptersBookTwo[3].after(chaptersBookTwo[6]);
+chaptersBookTwo[6].after(chaptersBookTwo[8]);
+chaptersBookTwo[7].after(chaptersBookTwo[9]);
+
+const chaptersBookFive = allBooks[5].querySelectorAll('li');
+
+chaptersBookFive[6].before(chaptersBookFive[2]);
+chaptersBookFive[3].before(chaptersBookFive[9]);
+chaptersBookFive[7].after(chaptersBookFive[5]);
+
+
+//6 в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место.
+
+const chaptersLists = document.querySelectorAll('ul');
+
+const missingChapter = document.createElement('li');
+missingChapter.textContent = 'Глава 8: За пределами ES6';
+chaptersLists[5].append(missingChapter);
+
+const chaptersBookSix = allBooks[2].querySelectorAll('li');
+chaptersBookSix[9].before(chaptersBookSix[10]);
